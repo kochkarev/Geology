d1a527e81ae6e4644eb4995b6117922ebdef9fb2
Alexey Kochkarev <alexey.kochkarev@gmail.com>
2019-11-07 02:43:03 +0300

    Metric iou for tensors added

--- metrics.py
+++ metrics.py
@@ -1,6 +1,6 @@
 #import plaidml.keras
 #plaidml.keras.install_backend()
-#from keras import backend as K
+from keras import backend as K
 import numpy as np
 
 def calc_metrics(mask_gt : np.ndarray, mask_pred : np.ndarray, metrics : list):
@@ -9,15 +9,15 @@ def calc_metrics(mask_gt : np.ndarray, mask_pred : np.ndarray, metrics : list):
     results = []
 
     for metric in metrics:
-        results.append(metrics_dict[metric](mask_gt, mask_pred))
+        results.append((metric, metrics_dict[metric](mask_gt, mask_pred)))
 
     return results
 
-# def iou(y_true, y_pred, smooth=1.):
-#     y_true_f = K.flatten(y_true)
-#     y_pred_f = K.flatten(y_pred)
-#     intersection = K.sum(y_true_f * y_pred_f)
-#     return (intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) - intersection + smooth)
+def iou_tf(y_true, y_pred, smooth=1.):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) - intersection + smooth)
 
 def iou(y_true, y_pred):
     smooth = 1.
