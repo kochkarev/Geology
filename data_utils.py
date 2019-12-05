76127a279d1d39aa91cebcda86a0ee21c2970146
Alexey Kochkarev <alexey.kochkarev@gmail.com>
2019-12-05 17:12:00 +0300

    Function for getting unmarked images names added, depricated functions removed

--- data_utils.py
+++ data_utils.py
@@ -18,6 +18,12 @@ def get_imgs_masks(path):
 
     return imgs_list, masks_list
 
+def get_unmarked_images(path, marked_path):
+    marked = glob.glob(os.path.join(marked_path, "*.jpg"))
+    all_images = glob.glob(os.path.join(path, "*.jpg"))
+    unmarked = set([os.path.basename(img) for img in all_images]) - set([os.path.basename(img) for img in marked])
+    return [img for img in all_images if os.path.basename(img) in unmarked]
+
 def get_pairs_from_paths(path):
 	images = glob.glob(os.path.join(path,"*.jpg"))
 	segmentations = glob.glob(os.path.join(path,"*_NEW.png")) 
@@ -51,64 +57,6 @@ def resize_imgs_masks(imgs, masks, num_layers=None, patch_size=None):
         new_masks.append(np.pad(mask, ((0, new_height - height), (0, new_width - width), (0, 0)), 'constant'))
 
     return new_imgs, new_masks
-
-# def make_patches(img, size=256):
-    
-#     assert (img.ndim == 3), ("Wrong input params: 3-channel input expected")
-#     assert (img.shape[0] % size == 0 and img.shape[1] % size == 0), ("Wrong input params: dimensions of image mod size must be zero")
-
-#     patches_list = []
-   
-#     for i in range(img.shape[0] // size):
-#         for j in range(img.shape[1] // size):
-#             patches_list.append(img[i*size : i*size+size, j*size : j*size+size, :])
-
-#     return np.stack(patches_list) 
-
-# def reconstruct_from_patches(img, org_img_size, size=256):
-
-#     assert (type(org_img_size) is tuple), ("Wrong input params: size must be a tuple")
-#     assert (img.ndim == 4), ("Wrong input params: 4-dim array expected")
-    
-#     i_max = (org_img_size[0] // size)
-#     j_max = (org_img_size[1] // size)
-       
-#     kk = 0
-#     img_bg = np.zeros((org_img_size[0], org_img_size[1], 3), dtype=img[0].dtype)
-    
-#     for i in range(i_max):
-#         for j in range(j_max):
-#             img_bg[i*size:i*size+size, j*size:j*size+size, :] = img[kk, :, :, :]
-#             kk += 1        
-
-#     return img_bg
-
-# def generate_patches_list(imgs, masks, patch_size):
-
-#     new_images = []
-#     new_masks = []
-    
-#     for img, mask in zip(imgs, masks):
-
-#         new_images.append(make_patches(img, patch_size))
-#         new_masks.append(make_patches(mask, patch_size))
-
-#     return new_images, new_masks
-
-# def convert_patches_list(imgs, masks):
-
-#     assert (imgs[0].shape[0] == masks[0].shape[0]), ("Wrong input params: # of images and masks must match")
-
-#     new_images = []
-#     new_masks = []
-
-#     for img, mask in zip(imgs, masks):
-
-#         for i in range(img.shape[0]):
-#             new_images.append(img[i, :, :, :])
-#             new_masks.append(mask[i, :, :, :])
-    
-#     return new_images, new_masks
 
 def split_to_patches(img, patch_size, offset, align=None):
 
