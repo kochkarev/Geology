a2422e7c64429aea59e22a7b4a95c9fd09fa2251
Alexey Kochkarev <alexey.kochkarev@gmail.com>
2020-03-06 19:53:03 +0300

    parse_dataset function added

--- scripts/make_dataset.py
+++ scripts/make_dataset.py
@@ -1,11 +1,41 @@
-def make_dataset():
+import os
+import shutil
+import fnmatch
+import xlrd
+import cv2 as cv
+import json
 
-    import os
-    import shutil
-    import fnmatch
-    import xlrd
-    import cv2 as cv
-    import json
+def parse_dataset(path):
+    
+    result = {}
+
+    wb = xlrd.open_workbook(path)
+    sheet_num = len(wb.sheet_names())
+
+    for sheet_idx in range(1, sheet_num):
+
+        sheet = wb.sheet_by_index(sheet_idx)
+        sheet.cell_value(0, 0)
+        dataset_name = (sheet.cell_value(0, 0)).split()[0]
+
+        marked = []
+        train = []
+        test = []
+
+        for i in range(sheet.nrows):
+            if type(sheet.cell_value(i, 0)) == type("aaa") and (sheet.cell_value(i, 0)).endswith(".jpg"):
+                marked.append(sheet.cell_value(i, 0))
+            if type(sheet.cell_value(i, 1)) == type("aaa") and (sheet.cell_value(i, 1)).endswith(".jpg"):
+                train.append(sheet.cell_value(i, 1))
+            if type(sheet.cell_value(i, 2)) == type("aaa") and (sheet.cell_value(i, 2)).endswith(".jpg"):
+                test.append(sheet.cell_value(i, 2))
+
+        result[dataset_name] = {"marked" : marked, "train" : train, "test" : test}
+
+    with open(os.path.join("input", "dataset.json"), 'w') as fp:
+        json.dump(result, fp, indent=4)
+
+def make_dataset():
 
     proj_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
     classes_path = os.path.join(proj_dir, 'input', 'UMNIK_2019', 'obj_class_to_machine_color.json')
@@ -45,6 +75,9 @@ def make_dataset():
     dataset_dir = os.path.join(proj_dir, 'input', 'dataset')
     path = os.path.join(proj_dir, 'input', 'UMNIK_2019', 'BoxA_DS1')
 
+    DIR = os.path.join(path, 'img')
+    dataset_size = len([name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))])
+
     try:
         os.mkdir(dataset_dir)
     except FileExistsError:
@@ -63,6 +96,7 @@ def make_dataset():
 
     names = list(dict.fromkeys(names))
 
+    marked_images = 0
     for fname in names:
         if fname.endswith('.jpg'):
             for file_name in os.listdir(os.path.join(path, 'img')):
@@ -72,6 +106,9 @@ def make_dataset():
                 if file_name.startswith(fname[:fname.find('.')]):    
                     shutil.copy(os.path.join(path, 'masks_machine', file_name), dataset_dir)
                     process_mask(os.path.join(dataset_dir, file_name))
+            marked_images += 1
+
+    print("{} marked images in dataset of {} images".format(marked_images, dataset_size))
 
 if __name__ == "__main__":
     make_dataset()
