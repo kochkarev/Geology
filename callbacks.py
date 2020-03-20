4f20e7795a85dd1e7529c27675dd06357bd231ff
Alexey Kochkarev <alexey.kochkarev@gmail.com>
2020-03-20 17:07:35 +0300

    refactoring

--- callbacks.py
+++ callbacks.py
@@ -100,30 +100,3 @@ class TestResults(Callback):
         plot_metrics_history(self.metrics_results)
         plot_per_class_history(self.metrics_per_cls_res)
         plot_lrs(self.lrs)
-
-# class LearningRateDecay:
-# 	def plot(self, epochs, title="Learning Rate Schedule"):
-# 		# compute the set of learning rates for each corresponding
-# 		# epoch
-# 		lrs = [self(i) for i in epochs]
-# 		# the learning rate schedule
-# 		plt.style.use("ggplot")
-# 		plt.figure()
-# 		plt.plot(epochs, lrs)
-# 		plt.title(title)
-# 		plt.xlabel("Epoch #")
-# 		plt.ylabel("Learning Rate")
-
-# class StepDecay(LearningRateDecay):
-# 	def __init__(self, initAlpha=0.01, factor=0.25, dropEvery=10):
-# 		# store the base initial learning rate, drop factor, and
-# 		# epochs to drop every
-# 		self.initAlpha = initAlpha
-# 		self.factor = factor
-# 		self.dropEvery = dropEvery
-# 	def __call__(self, epoch):
-# 		# compute the learning rate for the current epoch
-# 		exp = np.floor((1 + epoch) / self.dropEvery)
-# 		alpha = self.initAlpha * (self.factor ** exp)
-# 		# return the learning rate
-# 		return float(alpha)
