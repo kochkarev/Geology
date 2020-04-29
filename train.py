7b3674331a32b6343106c217a200621ea8f2cdf4
Alexey Kochkarev <alexey.kochkarev@gmail.com>
2020-04-29 15:25:34 +0300

    cmd line parser added

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ from data_utils import get_imgs_masks
 from tensorflow.keras.utils import to_categorical
 from unet import custom_unet
 from tensorflow.keras.callbacks import ModelCheckpoint, CSVLogger, ReduceLROnPlateau, EarlyStopping
-from tensorflow.keras.optimizers import Adam, SGD
+from tensorflow.keras.optimizers import Adam
 from metrics import iou, iou_multiclass
 from utils import plot_segm_history
 import os
@@ -16,8 +16,11 @@ import losses
 import shutil
 from config import classes_mask, train_params, classes_weights
 from sklearn.utils import class_weight
+from tensorflow.keras.models import load_model
+import pickle
+import argparse
 
-def train(n_classes, n_layers, n_filters, path, epochs, batch_size, patch_size, overlay, output_path, show_history=True):
+def train(n_classes, n_layers, n_filters, path, epochs, batch_size, patch_size, overlay, output_path, model_name = None, show_history=True):
     
     print('Loading images and masks..')
     t1 = time()
@@ -39,48 +42,44 @@ def train(n_classes, n_layers, n_filters, path, epochs, batch_size, patch_size, 
     # t2 = time()
     # class_weights = dict(enumerate(class_weights))
     # print(f'Weights computed in {t2 - t1} seconds')
+
+    weights = np.zeros((batch_size, patch_size, patch_size, n_classes), dtype=np.float32)
+    for cls_num in classes_weights:
+        weights[...,cls_num] = np.full((patch_size, patch_size), classes_weights[cls_num], dtype=np.float32)
     print(f'Weights: {classes_weights}')
 
     y_train = to_categorical(y_train, num_classes=n_classes).astype(np.uint8)
     y_test = to_categorical(y_test, num_classes=n_classes).astype(np.uint8)
 
     input_shape = (patch_size, patch_size, 3)
+    initial_epoch = 0
 
-    model = custom_unet(
-        input_shape,
-        n_classes=n_classes,
-        filters=n_filters,
-        use_batch_norm=True,
-        n_layers=n_layers,
-        output_activation='softmax'
-    )
-
-    model_filename = 'model_v1.h5'
-    callback_checkpoint = ModelCheckpoint(
-        model_filename, 
-        verbose=1, 
-        monitor='loss', 
-        save_best_only=True,
-    )
-
-    reduce_lr = ReduceLROnPlateau(
-        monitor='loss',
-        factor=0.2,
-        patience=5,
-        min_lr=0.00001,
-        verbose=1
-    )
-
-    # custom_loss = functools.partial(losses.weighted_dice_loss, weights=[classes_weights[i] for i in classes_weights.keys()])
+    custom_loss = functools.partial(losses.weighted_dice_loss, weights=weights)
     custom_loss = functools.partial(losses.cce_dice_loss)
-    custom_loss.__name__ = 'custom_loss'
+    custom_loss.__name__ = 'weighted_dice_loss'
 
-    model.compile(
-        optimizer=Adam(), 
-        loss = custom_loss,
-        metrics=[iou]
-    )
+    if model_name:
+        model = load_model(os.path.join(train_params["model_path"], model_name), custom_objects={'iou' : iou, 'weighted_dice_loss' : custom_loss})
+        initial_epoch = int(model_name.split('_')[1])
+        print(f'Model {model_name} loaded. Continue training from epoch {initial_epoch}')
+    else:
+        model = custom_unet(
+            input_shape,
+            n_classes=n_classes,
+            filters=n_filters,
+            use_batch_norm=True,
+            n_layers=n_layers,
+            output_activation='softmax'
+        )
 
+        model.compile(
+            optimizer=Adam(), 
+            loss = custom_loss,
+            metrics=[iou]
+        )
+
+    # Setting up callbacks
+    ######################################
     callback_test = TestResults(
         images=x_test, 
         masks=y_test,
@@ -101,28 +100,50 @@ def train(n_classes, n_layers, n_filters, path, epochs, batch_size, patch_size, 
         patience=7,
         restore_best_weights=True
     )
-    steps_per_epoch = 5
+
+    callback_checkpoint = ModelCheckpoint(
+        filepath=os.path.join(train_params["model_path"], 'model_{epoch:02d}_{loss:.2f}.hdf5'),
+        verbose=1, 
+        monitor='loss', 
+        save_best_only=True,
+        save_weights_only=False
+    )
+
+    reduce_lr = ReduceLROnPlateau(
+        monitor='loss',
+        factor=0.2,
+        patience=5,
+        min_lr=0.00001,
+        verbose=1
+    )
+
     csv_logger = CSVLogger('training.log')
+    ######################################
+
     train_generator = PatchGenerator(images=x_train, masks=y_train, names=train_names, patch_size=patch_size, batch_size=batch_size, full_augment=train_params["full_augment"])
+
+    steps_per_epoch = 5
     history = model.fit(
         iter(train_generator),
         steps_per_epoch=steps_per_epoch,
         epochs=epochs,
-        callbacks=[callback_checkpoint, callback_test, csv_logger, reduce_lr, early_stop]
+        callbacks=[callback_checkpoint, callback_test, csv_logger, reduce_lr, early_stop],
+        initial_epoch=initial_epoch
     )
 
-    for key in history.history:
-        print(key)
-
-    if show_history:
-        plot_segm_history(history, output_path)
+    plot_segm_history(history, output_path)
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--model', action='store')
+    args = parser.parse_args()
+
     if os.path.exists(train_params["output_path"]):
         shutil.rmtree(train_params["output_path"])
+    os.makedirs(train_params["model_path"], exist_ok=True)
 
     train(n_classes=len(classes_mask.keys()), n_layers=train_params["n_layers"], 
             n_filters=train_params["n_filters"], epochs=train_params["epochs"], 
             path=train_params["dataset_path"], batch_size=train_params["batch_size"],
             patch_size=train_params["patch_size"], overlay=train_params["overlay"],
-            output_path=train_params["output_path"])
+            output_path=train_params["output_path"], model_name=args.model)
