88a986052fa49492beb9601be26409ec9f68807f
Alexander Khvostikov <XUbiker@gmail.com>
2021-04-23 17:33:27 +0300

    set_gpu function added

--- utils/base.py
+++ utils/base.py
@@ -45,3 +45,19 @@ def prepare_experiment(out_path: Path) -> Path:
     exp_path = out_path / f'exp_{experiment_id}'
     exp_path.mkdir()
     return exp_path
+
+
+def set_gpu(gpu_index):
+	import tensorflow as tf
+	physical_devices  = tf.config.experimental.list_physical_devices('GPU')
+	print(f'Available GPUs: {len(physical_devices )}')
+	if physical_devices:
+		print(f'Choosing GPU #{gpu_index}')
+		try:
+			tf.config.experimental.set_visible_devices([physical_devices[gpu_index]], 'GPU')
+			logical_devices = tf.config.list_logical_devices('GPU')
+			assert len(logical_devices) == 1
+			print(f'Success. Now visible GPUs: {len(logical_devices)}')
+		except RuntimeError as e:
+			print('Something went wrong!')
+			print(e)
